9bea860179e69badecfdc83271d6ae2bb0d4592c
darth-xiaozhijun <15717061273@163.com>
2020-05-10 18:05:09 +0800

    discovery的使用

--- springcloud-provider-payment2/src/main/java/com/geekshow/springcloud/ProviderPaymentApplication2.java
+++ springcloud-provider-payment2/src/main/java/com/geekshow/springcloud/ProviderPaymentApplication2.java
@@ -2,10 +2,12 @@ package com.geekshow.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class ProviderPaymentApplication2 {
     public static void main(String[] args) {
         SpringApplication.run(ProviderPaymentApplication2.class, args);
