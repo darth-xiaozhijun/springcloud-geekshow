a6d99227f93d37d96a7faaf5081a15d2d3afd53a
darth-xiaozhijun <15717061273@163.com>
2020-06-14 15:19:43 +0800

    springcloud-alibaba sentinel服务降级

--- springcloud-alibaba-sentinel-service/src/main/java/com/geekshow/springcloud/controller/FlowerLimitController.java
+++ springcloud-alibaba-sentinel-service/src/main/java/com/geekshow/springcloud/controller/FlowerLimitController.java
@@ -1,9 +1,14 @@
 package com.geekshow.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
+
 @RestController
+@Slf4j
 public class FlowerLimitController {
     @GetMapping("/testA")
     public String testA() {
@@ -13,6 +18,28 @@ public class FlowerLimitController {
     @GetMapping("/testB")
     public String testB() {
 
+        log.info(Thread.currentThread().getName() + "\t" + "...testB");
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+//        try {
+//            TimeUnit.SECONDS.sleep(1);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        log.info("testD 测试RT");
+        log.info("testD 异常比例");
+        int age = 10/0;
+        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE()
+    {
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
     }
 }
